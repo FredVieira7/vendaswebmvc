6548463bc1913cf667b37e114f9cd19548ac75fd
Fred Vieira <fredvieira0709@gmail.com>
2021-11-10 11:04:11 -0300

    Implementando as entidades seller e salesrecord e segunda migration

--- Data/VendasWebMVCContext.cs
+++ Data/VendasWebMVCContext.cs
@@ -10,5 +10,7 @@ namespace VendasWebMVC.Data
         }
 
         public DbSet<VendasWebMVC.Models.Department> Department { get; set; }
+        public DbSet<VendasWebMVC.Models.Seller> Seller { get; set; }
+        public DbSet<VendasWebMVC.Models.SalesRecord> SalesRecord { get; set; }
     }
 }
--- Models/Department.cs
+++ Models/Department.cs
@@ -1,9 +1,35 @@
-﻿namespace VendasWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VendasWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) 
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
 
     }
 }
