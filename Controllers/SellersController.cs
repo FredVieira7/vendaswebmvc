a1cd0b566ef9e505b3015d99919555bedddb4aaa
Fred Vieira <fredvieira0709@gmail.com>
2021-11-10 14:28:42 -0300

    Implementando tela de cadastro de vendedores

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VendasWebMVC.Models;
 using VendasWebMVC.Services;
 
 namespace VendasWebMVC.Controllers
@@ -21,6 +22,19 @@ namespace VendasWebMVC.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.InsertSeller(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
