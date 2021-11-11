13dc52c8adfb8610b49e6718c01948ca89180889
Fred Vieira <fredvieira0709@gmail.com>
2021-11-11 11:18:25 -0300

    Implementando pagina detalhes dos vendedores e eager loading

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -66,5 +66,22 @@ namespace VendasWebMVC.Controllers
             _sellerService.RemoveSeller(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
